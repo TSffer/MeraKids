0211a46d9b38aaafb4c95e63f4e8e311694f9037
Luis Fernando Ttito <luis.ttito@ucsp.edu.pe>
2022-06-11 00:53:18 -0500

    se agregan los blogs restantes

--- FrontEndMeraKids/src/app/apps/blog/blog-data.ts
+++ FrontEndMeraKids/src/app/apps/blog/blog-data.ts
@@ -39,4 +39,54 @@ export const blogs: Blog[] = [
 		blogDetail:
 		'Est ipsum sint officia quis nulla nisi cupidatat aliquip nisi laboris eiusmod eiusmod aliquip do. Commodo elit excepteur occaecat irure Lorem sit nulla nulla sint duis incididunt. Nostrud ut do mollit et amet velit aute excepteur et culpa culpa velit. Cillum veniam officia anim cupidatat.',
 	},
+	{
+		id: 5,
+		link: 'https://www.instagram.com/p/CaqNPk2LzLN/?igshid=YmMyMTA2M2Y=',
+		image: 'assets/images/blog/404.jpg',
+		heading: 'Voluntariado',
+		subHeading: 'Fiorella Cherres, del equipo de Orientación y Consejería, nos cuenta que es lo más gusta de ser parte del #MeraTeam...',
+		blogDate: 'Marzo 3, 2022',
+		blogDetail:
+		'Est ipsum sint officia quis nulla nisi cupidatat aliquip nisi laboris eiusmod eiusmod aliquip do. Commodo elit excepteur occaecat irure Lorem sit nulla nulla sint duis incididunt. Nostrud ut do mollit et amet velit aute excepteur et culpa culpa velit. Cillum veniam officia anim cupidatat.',
+	},
+	{
+		id: 6,
+		link: 'https://www.instagram.com/p/CalNzsSrpDM/?igshid=YmMyMTA2M2Y=',
+		image: 'assets/images/blog/404.jpg',
+		heading: 'Día mundial de la cero discriminación',
+		subHeading: 'Hoy celebramos el #derecho de todas las personas a vivir una vida plena y digna...',
+		blogDate: 'Marzo 1, 2022',
+		blogDetail:
+		'Est ipsum sint officia quis nulla nisi cupidatat aliquip nisi laboris eiusmod eiusmod aliquip do. Commodo elit excepteur occaecat irure Lorem sit nulla nulla sint duis incididunt. Nostrud ut do mollit et amet velit aute excepteur et culpa culpa velit. Cillum veniam officia anim cupidatat.',
+	},
+	{
+		id: 7,
+		link: 'https://www.instagram.com/p/CaGVI8GrjQG/?igshid=YmMyMTA2M2Y=',
+		image: 'assets/images/blog/404.jpg',
+		heading: 'Voluntariado',
+		subHeading: 'Con mucho orgullo les presentamos a @franmottai , quien hoy nos cuenta un poco sobre cómo ha sido su experiencia en #Merakids...',
+		blogDate: 'Febrero 17, 2022',
+		blogDetail:
+		'Est ipsum sint officia quis nulla nisi cupidatat aliquip nisi laboris eiusmod eiusmod aliquip do. Commodo elit excepteur occaecat irure Lorem sit nulla nulla sint duis incididunt. Nostrud ut do mollit et amet velit aute excepteur et culpa culpa velit. Cillum veniam officia anim cupidatat.',
+	},
+	{
+		id: 8,
+		link: 'https://www.instagram.com/reel/CZxnYDUIJfW/?igshid=YmMyMTA2M2Y=',
+		image: 'assets/images/blog/404.jpg',
+		heading: 'Nuestro primer taller presencial',
+		subHeading: 'Queremos compartirles un poco de lo que fueron nuestros primeros #talleresemocionales...',
+		blogDate: 'Febrero 9, 2022',
+		blogDetail:
+		'Est ipsum sint officia quis nulla nisi cupidatat aliquip nisi laboris eiusmod eiusmod aliquip do. Commodo elit excepteur occaecat irure Lorem sit nulla nulla sint duis incididunt. Nostrud ut do mollit et amet velit aute excepteur et culpa culpa velit. Cillum veniam officia anim cupidatat.',
+	},
+	{
+		id: 9,
+		link: 'https://www.instagram.com/p/CZAwTBcrIYk/?igshid=YmMyMTA2M2Y=',
+		image: 'assets/images/blog/404.jpg',
+		heading: 'MeraLogros 2021',
+		subHeading: 'Hoy queremos compartir nuestros #MeraLogros del 2021...',
+		blogDate: 'Enero 21, 2022',
+		blogDetail:
+		'Est ipsum sint officia quis nulla nisi cupidatat aliquip nisi laboris eiusmod eiusmod aliquip do. Commodo elit excepteur occaecat irure Lorem sit nulla nulla sint duis incididunt. Nostrud ut do mollit et amet velit aute excepteur et culpa culpa velit. Cillum veniam officia anim cupidatat.',
+	},
 ];
